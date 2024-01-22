09ea91b034e8204559d362863d5e3391c0b4a615
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-23 02:00:53 +0800

    Added some words for the string

--- returning_count_of_given_substring_from_string.py
+++ returning_count_of_given_substring_from_string.py
@@ -4,7 +4,7 @@
 # Creating pseudocode
 
 # Creating a code for the given string
-str_x = "Ramil is so pogi, Ramil is maganda, Ramil is smart, Ramil is kind, Ramil is family centered, Ramil is God centered, Ramil love everyone around him, and Ramil is the best friend that you can have!"
+str_x = "Ramil is so pogi, Ramil is maganda, Ramil is smart, Ramil is kind, Ramil is family centered, Ramil is God centered, Ramil loves everyone around him, and Ramil is the best friend that you can have!"
 
 # Creating the code for the substring 
 cnt = str_x.count("Ramil")
