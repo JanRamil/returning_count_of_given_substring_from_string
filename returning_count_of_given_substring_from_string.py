c3a5be41fda2f299c1d3e66c8525c1aafb56160e
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-23 01:59:47 +0800

    Created a code to print on how many times did the string repeated

--- returning_count_of_given_substring_from_string.py
+++ returning_count_of_given_substring_from_string.py
@@ -4,6 +4,10 @@
 # Creating pseudocode
 
 # Creating a code for the given string
-str_x = "Emma is good developer. Emma is a writer"
+str_x = "Ramil is so pogi, Ramil is maganda, Ramil is smart, Ramil is kind, Ramil is family centered, Ramil is God centered, Ramil love everyone around him, and Ramil is the best friend that you can have!"
 
+# Creating the code for the substring 
+cnt = str_x.count("Ramil")
 
+# Creating  code to print on how many times did the string repeated
+print(cnt)
